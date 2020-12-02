3d13512f7f78ebd33da3c82dd0ec962025a90c3d
Dangereye <craig_puxty@talktalk.net>
2020-12-02 20:32:04 +0000

    Basic layout setup

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer>
+            <div className="container">
+                <p className="copyright">&copy; Dangereye 2020</p>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
---
+++ src/components/Main.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Main = () => {
+    return (
+        <main>
+            <div className="container">
+                main
+            </div>
+        </main>
+    )
+}
+
+export default Main
