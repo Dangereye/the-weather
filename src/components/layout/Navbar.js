44d33b672c321827f888885aecf4a506a369d13d
Craig Puxty <craig_puxty@talktalk.net>
2021-03-10 18:54:51 +0000

    Added Nav background change on scroll

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,10 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { BsFillGearFill } from "react-icons/bs";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const [background, setBackground] = useState();
+
+  const changeBg = () => {
+    if (window.pageYOffset > 100) {
+      setBackground(true);
+    } else {
+      setBackground(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", changeBg);
+    return () => {
+      window.removeEventListener("scroll", changeBg);
+    };
+  });
+
+  const bgColor = { background: background ? "#000" : "transparent" };
+
   return (
-    <nav>
+    <nav style={bgColor}>
       <div className="container">
         <Link to="/" className="logo">
           the weather
