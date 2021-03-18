cb3ac69280a5e5ccf23ae499e7607b60e8b6a12f
Craig Puxty <craig_puxty@talktalk.net>
2021-03-18 13:23:27 +0000

    Updates settings page, looks pretty now.

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -1,9 +1,21 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import RadioButton from "../components/shared/RadioButton";
 import { WeatherContext } from "../contexts/WeatherContext";
+import { FaTemperatureLow } from "react-icons/fa";
+import { BsChevronDown } from "react-icons/bs";
+import { IoMdSpeedometer } from "react-icons/io";
+import { IoWaterOutline } from "react-icons/io5";
+import { RiWindyFill } from "react-icons/ri";
+import { MdVisibility } from "react-icons/md";
 
 const Settings = () => {
   const { state, dispatch } = useContext(WeatherContext);
+
+  const [isTemp, setIsTemp] = useState(false);
+  const [isSpeed, setIsSpeed] = useState(false);
+  const [isDistance, setIsDistance] = useState(false);
+  const [isPrecipitation, setIsPrecipitation] = useState(false);
+  const [isPressure, setIsPressure] = useState(false);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -17,101 +29,210 @@ const Settings = () => {
     <section className="settings">
       <div className="container">
         <h2>Settings</h2>
-        <h3>Temperature</h3>
-        <div className="radio-group">
-          <RadioButton
-            id="1"
-            changed={handleChange}
-            isSelected={state.settings.temp === "celsius"}
-            name="temp"
-            label="Celsius"
-            value="celsius"
-          />
-          <RadioButton
-            id="2"
-            changed={handleChange}
-            isSelected={state.settings.temp === "fahrenheit"}
-            name="temp"
-            label="Fahrenheit"
-            value="fahrenheit"
-          />
+        {/* Temperature */}
+        <div
+          className={isTemp ? "setting active" : "setting"}
+          onClick={() => setIsTemp(!isTemp)}
+        >
+          <div className="setting-content">
+            <div className="setting-icon">
+              <FaTemperatureLow />
+            </div>
+            <div className="setting-text">
+              <h4>Temperature</h4>
+              <p>{state.settings.temp}</p>
+            </div>
+          </div>
+          <BsChevronDown className="status" />
         </div>
-        <h3>Speed</h3>
-        <div className="radio-group">
-          <RadioButton
-            id="3"
-            changed={handleChange}
-            isSelected={state.settings.speed === "mph"}
-            name="speed"
-            label="mph"
-            value="mph"
-          />
-          <RadioButton
-            id="4"
-            changed={handleChange}
-            isSelected={state.settings.speed === "kph"}
-            name="speed"
-            label="kph"
-            value="kph"
-          />
+        {isTemp && (
+          <div className="setting-options">
+            <div className="option">
+              <RadioButton
+                id="celsius"
+                changed={handleChange}
+                isSelected={state.settings.temp === "celsius"}
+                name="temp"
+                label="Celsius"
+                value="celsius"
+              />
+            </div>
+            <div className="option">
+              <RadioButton
+                id="fahrenheit"
+                changed={handleChange}
+                isSelected={state.settings.temp === "fahrenheit"}
+                name="temp"
+                label="Fahrenheit"
+                value="fahrenheit"
+              />
+            </div>
+          </div>
+        )}
+
+        {/* Wind speed */}
+        <div
+          className={isSpeed ? "setting active" : "setting"}
+          onClick={() => setIsSpeed(!isSpeed)}
+        >
+          <div className="setting-content">
+            <div className="setting-icon">
+              <IoMdSpeedometer />
+            </div>
+            <div className="setting-text">
+              <h4>Wind speed</h4>
+              <p>{state.settings.speed}</p>
+            </div>
+          </div>
+          <BsChevronDown className="status" />
         </div>
-        <h3>Distance</h3>
-        <div className="radio-group">
-          <RadioButton
-            id="5"
-            changed={handleChange}
-            isSelected={state.settings.distance === "miles"}
-            name="distance"
-            label="miles"
-            value="miles"
-          />
-          <RadioButton
-            id="6"
-            changed={handleChange}
-            isSelected={state.settings.distance === "km"}
-            name="distance"
-            label="kilometer"
-            value="km"
-          />
+        {isSpeed && (
+          <div className="setting-options">
+            <div className="option">
+              <RadioButton
+                id="mph"
+                changed={handleChange}
+                isSelected={state.settings.speed === "mph"}
+                name="speed"
+                label="Miles per hour"
+                value="mph"
+              />
+            </div>
+            <div className="option">
+              <RadioButton
+                id="kph"
+                changed={handleChange}
+                isSelected={state.settings.speed === "kph"}
+                name="speed"
+                label="Kilometers per hour"
+                value="kph"
+              />
+            </div>
+          </div>
+        )}
+
+        {/* Visibility distance */}
+        <div
+          className={isDistance ? "setting active" : "setting"}
+          onClick={() => setIsDistance(!isDistance)}
+        >
+          <div className="setting-content">
+            <div className="setting-icon">
+              <MdVisibility />
+            </div>
+            <div className="setting-text">
+              <h4>Visibility distance</h4>
+              <p>{state.settings.distance}</p>
+            </div>
+          </div>
+          <BsChevronDown className="status" />
         </div>
-        <h3>Volume</h3>
-        <div className="radio-group">
-          <RadioButton
-            id="7"
-            changed={handleChange}
-            isSelected={state.settings.precipitation === "mm"}
-            name="precipitation"
-            label="millimeters"
-            value="mm"
-          />
-          <RadioButton
-            id="8"
-            changed={handleChange}
-            isSelected={state.settings.precipitation === "in"}
-            name="precipitation"
-            label="inches"
-            value="in"
-          />
+        {isDistance && (
+          <div className="setting-options">
+            <div className="option">
+              <RadioButton
+                id="miles"
+                changed={handleChange}
+                isSelected={state.settings.distance === "miles"}
+                name="distance"
+                label="Miles"
+                value="miles"
+              />
+            </div>
+            <div className="option">
+              <RadioButton
+                id="kilometers"
+                changed={handleChange}
+                isSelected={state.settings.distance === "km"}
+                name="distance"
+                label="Kilometers"
+                value="km"
+              />
+            </div>
+          </div>
+        )}
+
+        {/* Precipitation volume */}
+        <div
+          className={isPrecipitation ? "setting active" : "setting"}
+          onClick={() => setIsPrecipitation(!isPrecipitation)}
+        >
+          <div className="setting-content">
+            <div className="setting-icon">
+              <IoWaterOutline />
+            </div>
+            <div className="setting-text">
+              <h4>Precipitation volume</h4>
+              <p>{state.settings.precipitation}</p>
+            </div>
+          </div>
+          <BsChevronDown className="status" />
         </div>
-        <h3>Pressure</h3>
-        <div className="radio-group">
-          <RadioButton
-            id="9"
-            changed={handleChange}
-            isSelected={state.settings.pressure === "mb"}
-            name="pressure"
-            label="millibars"
-            value="mb"
-          />
-          <RadioButton
-            id="10"
-            changed={handleChange}
-            isSelected={state.settings.pressure === "in"}
-            name="pressure"
-            label="inches"
-            value="in"
-          />
+        {isPrecipitation && (
+          <div className="setting-options">
+            <div className="option">
+              <RadioButton
+                id="millimeters"
+                changed={handleChange}
+                isSelected={state.settings.precipitation === "mm"}
+                name="precipitation"
+                label="Millimeters"
+                value="mm"
+              />
+            </div>
+            <div className="option">
+              <RadioButton
+                id="inches"
+                changed={handleChange}
+                isSelected={state.settings.precipitation === "in"}
+                name="precipitation"
+                label="Inches"
+                value="in"
+              />
+            </div>
+          </div>
+        )}
+
+        {/* Air pressure */}
+        <div
+          className={isPressure ? "setting active" : "setting"}
+          onClick={() => setIsPressure(!isPressure)}
+        >
+          <div className="setting-content">
+            <div className="setting-icon">
+              <RiWindyFill />
+            </div>
+            <div className="setting-text">
+              <h4>Air pressure</h4>
+              <p>{state.settings.pressure}</p>
+            </div>
+          </div>
+          <BsChevronDown className="status" />
         </div>
+        {isPressure && (
+          <div className="setting-options">
+            <div className="option">
+              <RadioButton
+                id="millibars"
+                changed={handleChange}
+                isSelected={state.settings.pressure === "mb"}
+                name="pressure"
+                label="Millibars"
+                value="mb"
+              />
+            </div>
+            <div className="option">
+              <RadioButton
+                id="pressure-inches"
+                changed={handleChange}
+                isSelected={state.settings.pressure === "in"}
+                name="pressure"
+                label="Inches"
+                value="in"
+              />
+            </div>
+          </div>
+        )}
       </div>
     </section>
   );
